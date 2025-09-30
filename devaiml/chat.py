f753ead29d6b3fb0f9ae4d9cc40213bd4d3cd669
Mitresh <85941851+Mitresh-J@users.noreply.github.com>
2025-09-30 22:13:13 +0530

    Add files via upload

---
+++ devaiml/chat.py
@@ -0,0 +1,40 @@
+#
+from groq import Groq
+import json
+import concurrent.futures
+
+client = Groq(
+    api_key="MY_API_KEY",
+)
+
+def get_response(i, prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="openai/gpt-oss-120b"
+    )
+    response = chat_completion.choices[0].message.content
+    return (f"prompt_{i}", [prompt, response])
+
+DATA = {}
+
+# open file prompts.txt and read its content
+with open("prompts.txt", "r") as file:
+    prompts = file.readlines()
+
+    # parallel requests for each prompt in prompts to save time
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_response, i, prompts[i]) for i in range(len(prompts))]
+        for future in concurrent.futures.as_completed(futures):
+            key, value = future.result()
+            DATA[key] = value
+
+
+with open("responses.json", "w") as file:
+    json.dump(DATA, file, indent=4) 
+
+        
